e9e113a9e39e9ca1cbba84d082611c5436a67416
Dhanamjaya5 <dhanamjaysri@gmail.com>
2022-11-02 05:53:40 +0530

    changes

--- src/Components/Common/input/Input.jsx
+++ src/Components/Common/input/Input.jsx
@@ -4,7 +4,8 @@ import {useNavigate} from 'react-router-dom';
 import {Button, Stack,TextField} from '@mui/material';
 import {makeStyles} from '@mui/styles'
 import axios from 'axios';
-
+import InfoIcon from '@mui/icons-material/Info';
+import TextInputLogo from '../../pictures/undraw_text_field_htlv.svg';
 
 function Input(){
     const [data,setData]=useState(false);
@@ -34,8 +35,18 @@ function Input(){
                     <a className='options-text'>Blog</a>
                 </section>
             </section>
-            <section>
-                <section className='form-view'> 
+            <section className='input-image-view'>
+                <section className='form-view'>
+                    <section className='info-view'>
+                        <section style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
+                            <InfoIcon style={{alignSelf:'center'}}/>
+                            <a style={{alignSelf:'center',marginLeft:'10px'}}>Text length: 50 - 500 words. Supports English</a>
+                        </section>
+                        
+                    </section> 
+                    <section className='input-caption-view'>
+                        <a>File uploads will be added soon, hang in there!</a>
+                    </section>
                     <form noValidate autoComplete='off' onSubmit={handleSubmit} style={{display:'flex',flexDirection:'column'}}>   
                         <TextField 
                             onChange={(e)=>setData(e.target.value)}
@@ -47,12 +58,23 @@ function Input(){
                             style={{width:'600px', background:'#F2F2F7'}}
                             error={dataError}/>
                         <div style={{marginTop:'20px'}}>    
-                        <Button className='submit-button-view'type='submit'><a>Submit</a></Button>
+                        <Button type="submit" style=
+                            {{backgroundColor:'#3A4C8E',
+                            color:'white',
+                            width:'600px',
+                            borderRadius:'15px',
+                            marginTop:'10px'}}><a>Submit</a></Button>
                         </div>
                     </form>
                     
                 </section>
-                <section className='image'></section>
+                <section className='image'>
+                <img src={TextInputLogo} 
+                    alt="Input Logo" 
+                    width="600" 
+                    height="430"
+                    />
+                </section>
             </section>
             <section className='output-view'>
 
